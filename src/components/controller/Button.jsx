836bacb31132cc0cb2828b4ec31ecf78511c0662
Sinh Vu <sinhvusinh1997@gmail.comm>
2021-04-28 19:51:20 +0700

    update UI controller

--- src/components/controller/Button.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Button = () => {
-  return (
-    <input type="button" value="" />
-  )
-};
-
-export default Button;
--- src/components/controller/IndexController.jsx
+++ src/components/controller/IndexController.jsx
@@ -1,14 +1,40 @@
-import React from 'react';
-import Button from './Button';
-import Search from './Search';
+import React, { useState } from 'react';
 
 
 const IndexController = () => {
+  const [searchID, setSearchID] = useState('');
+  const [searchName, setSearchName] = useState('');
+
+  const handleChangeSearchID = e => setSearchID(e.target.value);
+
+  const handleChangeSearchName = e => setSearchName(e.target.value);
 
 
   return (
     <div className="wrapper-controller">
-      this is controller
+      <input
+        type="text"
+        name="searchByID"
+        id="searchByID"
+        placeholder="Find by ID ..."
+        autoComplete="off"
+        value={searchID}
+        onChange={e => handleChangeSearchID(e)}
+      />
+
+      <input
+        type="text"
+        name="searchByName"
+        id="searchByName"
+        placeholder="Find by Name ..."
+        autoComplete="off"
+        value={searchName}
+        onChange={e => handleChangeSearchName(e)}
+      />
+
+      <button>
+        <i className="fas fa-plus"></i>
+      </button>
     </div>
   )
 };
